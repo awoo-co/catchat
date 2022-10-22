9dfd595f0eef8ab730eecb1b073409af4d662cad
james <jkuyt39@gmail.com>
2022-10-22 15:56:11 +0000

    meow

--- script.js
+++ script.js
@@ -59,8 +59,8 @@ drone.on('error', error => {
 });
 
 function getRandomName() {
-  const adjs = ["customfirstnamehere"]
-  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+  const adjs = ["teslakitty" "human" ]
+  const nouns = ["dog" "cat" "catman"];
   return (
     adjs[Math.floor(Math.random() * adjs.length)] +
     "_" +
