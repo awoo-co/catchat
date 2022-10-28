fa78512bd144ef1a418e1cdd092c46189454184b
james <jkuyt39@gmail.com>
2022-10-28 21:35:17 +0000

    meow

--- script.js
+++ script.js
@@ -130,3 +130,9 @@ function addMessageToListDOM(text, member) {
   }
 }
 
+const interaction = document.querySelector('.c-interaction');
+const toggleButton = document.querySelector('.c-interaction__toggle')
+
+toggleButton.addEventListener('click',() => {
+  interaction.classList.toggle('c-interaction__options')
+})
