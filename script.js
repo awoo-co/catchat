78feb969d23a102a4e29568f6c3551310547f6f0
james <jkuyt39@gmail.com>
2022-11-13 21:23:09 +0000

    meow

--- script.js
+++ script.js
@@ -139,3 +139,15 @@ Notification.requestPermission()
 
 
 
+import { createPicker } from 'picmo';
+
+// The picker must have a root element to insert itself into
+const rootElement = document.querySelector('#pickerContainer');
+
+// Create the picker
+const picker = createPicker({ rootElement });
+
+// The picker emits an event when an emoji is selected. Do with it as you will!
+picker.addEventListener('emoji:select', event => {
+  console.log('Emoji selected:', event.emoji);
+});
