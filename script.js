5b7f59a8759d2812770a09cf61440515db7a7d6d
james <jkuyt39@gmail.com>
2022-11-13 21:12:07 +0000

    meow

--- script.js
+++ script.js
@@ -134,3 +134,18 @@ function addMessageToListDOM(text, member) {
 
 Notification.requestPermission()
 
+const image_input = document.querySelector("#image-input");
+
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+
+
+
+
+
