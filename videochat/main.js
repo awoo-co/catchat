64ffaaf0053b8eeea4b7cf4ff92b7d7f17f917fb
shadowcat143 <jameskuyt655@gmail.com>
2022-12-27 19:09:57 +0000

    meow

--- videochat/main.js
+++ videochat/main.js
@@ -1,5 +1,5 @@
 const APP_ID = "5f10b8d038114e4494671eba6636a671"
-const TOKEN = "007eJxTYHhneLl1rqSwiOyEtvY+Q5WJ358tOeXW8kW+3Pri1OAZR7MUGEzTDA2SLFIMjC0MDU1STUwsTczMDVOTEs3MjM0Sgcz2ZyuTGwIZGUxu87MwMkAgiM/CkJuaX87AAAA5Nh8b"
+const TOKEN = "007eJxTYHgkuvSR6dznMVK7ne5MY/E79/xp/mTD/Qm9rnM7f8xImCCqwGCaZmiQZJFiYGxhaGiSamJiaWJmbpialGhmZmyWCGT+cFqd3BDIyNAx/SojIwMEgvgsDLmp+eUMDACtXyDI"
 const CHANNEL = "meow"
 
 const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
