15a9d94f56827a054190ba0b721dbeb98a5c8252
james <jkuyt39@gmail.com>
2023-03-18 22:18:22 +0000

    meow

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,15 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyASQEPMe2tBt8h4ohflMThKZ74Qj9LolM0",
+  authDomain: "this-te.firebaseapp.com",
+  databaseURL: "https://this-te-default-rtdb.firebaseio.com",
+  projectId: "this-te",
+  storageBucket: "this-te.appspot.com",
+  messagingSenderId: "815151260683",
+  appId: "1:815151260683:web:432e743fde469bc53d530e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+﻿const firebaseConfig = {
+  apiKey: "AIzaSyASQEPMe2tBt8h4ohflMThKZ74Qj9LolM0",
+  authDomain: "this-te.firebaseapp.com",
+  projectId: "this-te",
+  storageBucket: "this-te.appspot.com",
+  messagingSenderId: "815151260683",
+  appId: "1:815151260683:web:432e743fde469bc53d530e"
+};
