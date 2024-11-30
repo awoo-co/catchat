5af7cc502493ddcfdeb3daeda7ce2704c5777680
teslakitty1 <jameskuyt655@gmail.com>
2024-11-30 06:54:59 -0500

    catchat migrated

--- firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-﻿const firebaseConfig = {
-  apiKey: "AIzaSyASQEPMe2tBt8h4ohflMThKZ74Qj9LolM0",
-  authDomain: "this-te.firebaseapp.com",
-  databaseURL: "https://this-te-default-rtdb.firebaseio.com",
-  projectId: "this-te",
-  storageBucket: "this-te.appspot.com",
-  messagingSenderId: "815151260683",
-  appId: "1:815151260683:web:432e743fde469bc53d530e"
-};
