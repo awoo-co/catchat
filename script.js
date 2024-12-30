24925db2808271cd96df9f14efec4b7b195215fd
James <jameskuyt655@gmail.com>
2024-12-29 22:07:22 -0500

    fix: update service worker registration paths for Catchat applications

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 document.getElementById('server1').addEventListener('click', function() {
-    window.location.href = 'catchat1/index.html';
+  window.location.href = 'catchat1/index.html';
 });
 
 document.getElementById('server2').addEventListener('click', function() {
-    window.location.href = 'catchat2/index.html';
+  window.location.href = 'catchat2/index.html';
 });
 
 document.getElementById('server3').addEventListener('click', function() {
-    window.location.href = 'catchat3/index.html';
+  window.location.href = 'catchat3/index.html';
 });
 
-
-
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js').then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch(error => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(error => {
+      console.log('ServiceWorker registration failed: ', error);
     });
-  }
+  });
+}
